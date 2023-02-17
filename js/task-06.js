44b18cb597cde1c2f06d358d9b5b3a799c5b84a6
Rdk001 <rmaslenchenko95@gmail.com>
2023-02-17 18:44:57 +0100

    добавил удаление класса "valid" в 6 задании

--- js/task-06.js
+++ js/task-06.js
@@ -7,6 +7,7 @@ function onCorrectNumberOfCharacters(event) {
     event.currentTarget.value.length !==
     Number(event.currentTarget.dataset.length)
   ) {
+    event.currentTarget.classList.remove("valid");
     event.currentTarget.classList.add("invalid");
   } else {
     event.currentTarget.classList.add("valid");
